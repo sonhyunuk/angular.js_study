8c59e8f75d85209cf1508bec964176dbc12745db
sonhyunuk <59695105+sonhyunuk@users.noreply.github.com>
2021-03-22 21:34:31 +0900

    테이블에 데이터 표시

--- demo/src/main/resources/static/pages/user/user.js
+++ demo/src/main/resources/static/pages/user/user.js
@@ -1,11 +1,8 @@
-$(document).ready(function () {
-    var data = [
-        {
-          "name" :  "Tiger Nixon"
-        },
-        {
-            "name": "Garrett Winter"
-        }
-]
-
-});
+angular.module('app')
+  .controller("ngController", function ($scope) {
+    $scope.arrData = [
+      { id: 1, name: "Apple" },
+      { id: 2, name: "Banana" },
+      { id: 3, name: "Orange" },
+      { id: 4, name: "Mango" }];
+  });
